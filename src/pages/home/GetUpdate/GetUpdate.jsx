80d9a3a52c36cae3c51d1ebe81048d392ead4dc0
Sultan Mahmud <mdsultanmahmud.bd00@gmail.com>
2022-11-06 22:11:52 +0600

    added footer

--- src/pages/home/GetUpdate/GetUpdate.jsx
+++ src/pages/home/GetUpdate/GetUpdate.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import shoe from '../../../assets/shoe.jpg'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faTruck, faSackDollar, faHeadset,faHandHoldingHand} from '@fortawesome/free-solid-svg-icons'
 
 const GetUpdate = () => {
     return (
@@ -24,8 +24,43 @@ const GetUpdate = () => {
                     </div>
                 </div>
             </div>
-            <div>
-
+            <div className='grid grid-cols-1 md:grid-cols-2'>
+                <div className='flex items-center p-8 m-2'>
+                    <div className='mr-5'>
+                    <FontAwesomeIcon className='w-10 h-10' icon={faTruck} />
+                    </div>
+                    <div>
+                        <h4 className='text-xl font-bold text-zinc-800'>Fast & Secure Delivery</h4>
+                        <p className='text-grey text-sm font-semibold text-zinc-400'>Tell About Your Service</p>
+                    </div>
+                </div>
+                <div className='flex items-center p-8 m-5'>
+                    <div className='mr-5'>
+                    <FontAwesomeIcon className='w-10 h-10' icon={faSackDollar} />
+                    </div>
+                    <div>
+                        <h4 className='text-xl font-bold text-zinc-800'>Money Back Guarantee</h4>
+                        <p className='text-grey text-sm font-semibold text-zinc-400'>Within 7 days.</p>
+                    </div>
+                </div>
+                <div className='flex items-center p-8 m-5'>
+                    <div className='mr-5'>
+                    <FontAwesomeIcon className='w-10 h-10' icon={faHeadset} />
+                    </div>
+                    <div>
+                        <h4 className='text-xl font-bold text-zinc-800'>Pro Quality Support</h4>
+                        <p className='text-grey text-sm font-semibold text-zinc-400'>24/7 Live support.</p>
+                    </div>
+                </div>
+                <div className='flex items-center p-8 m-5'>
+                    <div className='mr-5'>
+                    <FontAwesomeIcon className='w-10 h-10' icon={faHandHoldingHand} />
+                    </div>
+                    <div>
+                        <h4 className='text-xl font-bold text-zinc-800'>24 Hour Return Policy</h4>
+                        <p className='text-grey text-sm font-semibold text-zinc-400'>No question ask.</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
